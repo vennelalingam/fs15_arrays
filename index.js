5bc33cff838225b99a5a2b43b03cf552793796ea
vennelalingam <lingamvennela@gmail.com>
2023-05-04 19:07:09 +0530

    arrays

---
+++ index.js
@@ -0,0 +1,15 @@
+/*------------------
+1)
+Create an array called "numbers"
+and include the numbers zero
+through five. 
+
+The console.log statement is checking
+if you are correct.
+------------------*/
+
+let numbers = []
+
+numbers.push(0, 1, 2, 3, 4, 5);
+
+console.log(numbers); // [0, 1, 2, 3, 4, 5]
